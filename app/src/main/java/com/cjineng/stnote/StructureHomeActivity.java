f675dd72168039a35c883c2b54aab40a88edc5fd
cjinsteng <fpig503@gmail.com>
2020-06-06 19:11:56 +0900

    Initial commit

---
+++ app/src/main/java/com/cjineng/stnote/StructureHomeActivity.java
@@ -0,0 +1,14 @@
+package com.cjineng.stnote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StructureHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_structure_home);
+    }
+}
